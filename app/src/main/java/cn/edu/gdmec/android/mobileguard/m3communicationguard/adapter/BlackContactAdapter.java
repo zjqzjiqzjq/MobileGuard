ed85772b957c46ffbfb2d38809f04eb16c2675fe
630815708 <630815708@qq.com>
2017-11-12 18:58:08 +0800

    添加黑名单类型

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
@@ -27,6 +27,7 @@ public class BlackContactAdapter  extends BaseAdapter{
     class ViewHolder{
         TextView mNameTV;
         TextView mModeTV;
+        TextView mTypeTV;
         View mContactImgv;
         View mDeleteView;
     }
@@ -66,6 +67,7 @@ public class BlackContactAdapter  extends BaseAdapter{
             holder = new ViewHolder();
             holder.mNameTV = (TextView) view.findViewById(R.id.tv_black_name);
             holder.mModeTV = (TextView) view.findViewById(R.id.tv_black_mode);
+            holder.mTypeTV = (TextView) view.findViewById(R.id.tv_black_type);
             holder.mContactImgv =  view.findViewById(R.id.view_black_icon);
             holder.mDeleteView =  view.findViewById(R.id.view_black_delete);
             view.setTag(holder);
@@ -75,8 +77,10 @@ public class BlackContactAdapter  extends BaseAdapter{
         }
         holder.mNameTV.setText(contactInfos.get(i).contactName + "(" + contactInfos.get(i).phoneNumber + ")");
         holder.mModeTV.setText(contactInfos.get(i).getModeString(contactInfos.get(i).mode));
+        holder.mTypeTV.setText(contactInfos.get(i).blackType);
         holder.mNameTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
         holder.mModeTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
+        holder.mTypeTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
         holder.mContactImgv.setBackgroundResource(R.drawable.brightpurple_contact_icon);
         holder.mDeleteView.setOnClickListener(new View.OnClickListener(){
             @Override
